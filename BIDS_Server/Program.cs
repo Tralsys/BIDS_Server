5ee732a726286bad7d4c7262f4c34cebb3217636
Tetsu Otter <tetsu_otter@aol.com>
2018-10-30 22:06:59 +0900

    State送信機能を実装 JavaScript側を作成

--- BIDS_Server/Program.cs
+++ BIDS_Server/Program.cs
@@ -4,12 +4,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Runtime.InteropServices;
+using TR.BIDScs;
 namespace BIDS_Server
 {
   class Program
   {
+    /*BIDS Server
+     * HTML+JavaScriptとの間でWebSocketでの通信。
+     */
     static void Main(string[] args)
     {
+      Console.WriteLine("BIDS WebSocketサーバーアプリケーション");
+      Console.WriteLine("ver : 0.0.1(Alpha)");
+      Console.WriteLine("ブラウザを開き、「http://trtec.s1006.xrea.com/TCP_SkyWay.html」にアクセスしてください。");
+
+      SocketDo();
+
+      do Console.WriteLine("exitを入力することで終了できます。"); while (Console.ReadLine() != "exit");
+    }
+
+
+
+    static async void SocketDo()
+    {
+      HttpListener HL = new HttpListener();
+      HL.Prefixes.Add("http://localhost:12835/");
+      HL.Start();
+      while (true)
+      {
+        HttpListenerContext HLC = await HL.GetContextAsync();
+        if (HLC.Request.IsWebSocketRequest)
+        {
+          Console.WriteLine("WebSocket接続要求を確認しました。");
+          WebSocketContext WSC = null;
+          try
+          {
+            WSC = await HLC.AcceptWebSocketAsync(subProtocol: null);
+          }catch(Exception e)
+          {
+            Console.WriteLine("Socket開始エラー : " + e.Message);
+            HLC.Response.StatusCode = 500;
+            HLC.Response.Close();
+            Console.WriteLine("再度接続試行を行います。");
+          }
+          Console.WriteLine("WebSocket接続を開始します。");
+          WebSocket WS = WSC.WebSocket;
+          try
+          {
+            using (Pipe Pp = new Pipe())
+            {
+              Pp.Open();
+              byte[] SendArray = new byte[64];
+              Pipe.StateData ST1 = new Pipe.StateData();
+              Pipe.State2Data ST2 = new Pipe.State2Data();
+              while (WS.State == WebSocketState.Open)
+              {
+                if (!Equals(ST1, Pp.NowState) || !Equals(ST2, Pp.NowState2))
+                {
+                  ST1 = Pp.NowState;
+                  ST2 = Pp.NowState2;
+                  BitConverter.GetBytes(2).CopyTo(SendArray, 0);
+                  BitConverter.GetBytes(ST1.Location).CopyTo(SendArray, 4);
+                  BitConverter.GetBytes(ST1.Speed).CopyTo(SendArray, 12);
+                  BitConverter.GetBytes(ST2.BC).CopyTo(SendArray, 16);
+                  BitConverter.GetBytes(ST2.MR).CopyTo(SendArray, 20);
+                  BitConverter.GetBytes(ST2.ER).CopyTo(SendArray, 24);
+                  BitConverter.GetBytes(ST2.BP).CopyTo(SendArray, 28);
+                  BitConverter.GetBytes(ST2.SAP).CopyTo(SendArray, 32);
+                  BitConverter.GetBytes(ST1.Current).CopyTo(SendArray, 36);
+                  BitConverter.GetBytes(ST1.PowerNotch).CopyTo(SendArray, 40);
+                  BitConverter.GetBytes(ST1.BrakeNotch).CopyTo(SendArray, 44);
+                  BitConverter.GetBytes((sbyte)ST1.Reverser).CopyTo(SendArray, 48);
+                  BitConverter.GetBytes(ST2.IsDoorClosed).CopyTo(SendArray, 49);
+                  BitConverter.GetBytes((byte)ST2.Hour).CopyTo(SendArray, 50);
+                  BitConverter.GetBytes((byte)ST2.Minute).CopyTo(SendArray, 51);
+                  BitConverter.GetBytes((byte)ST2.Second).CopyTo(SendArray, 52);
+                  BitConverter.GetBytes((byte)ST2.Millisecond).CopyTo(SendArray, 53);
+                  BitConverter.GetBytes(0xFEFEFEFE).CopyTo(SendArray, 60);
+                  await WS.SendAsync(new ArraySegment<byte>(SendArray, 0, SendArray.Length), WebSocketMessageType.Binary, true, CancellationToken.None);
+                }
+                Thread.Sleep(10);
+              }
+            }
+          }catch(Exception e)
+          {
+            Console.WriteLine("Socket通信エラー : " + e.Message);
+            HLC.Response.StatusCode = 500;
+            HLC.Response.Close();
+          }
+          finally { if (WS != null) WS.Dispose(); }
+          Console.WriteLine("WebSocket接続が終了しました。再度接続を試行します。");
+        }
+        else
+        {
+          Console.WriteLine("WebSocket接続ではありませんでした。");
+          HLC.Response.StatusCode = 400;
+          HLC.Response.Close();
+        }
+      }
     }
   }
 }
---
+++ BIDS_Server/tcp_skyway.js
@@ -0,0 +1,75 @@
+﻿const peer = new Peer({
+    key: _MYAPIKEY_,
+    debug: 0,
+});
+const DataConnection = {};
+
+peer.on('open', id => {
+    console.log(id);
+    var PID = document.getElementById('MyPeerID');
+    PID.innerHTML = 'Peer ID : ' + id;
+});
+
+var WSC = new WebSocket('ws://localhost:12835');
+WSC.close();
+function WSConnecter() {
+    WSC = new WebSocket("ws://localhost:12835");
+    WSC.binaryType = 'arraybuffer';
+    WSC.onopen = function () {
+        console.log('WebSocket Open');
+        var IsCDTexto = document.getElementById('IsSocketConnected');
+        IsCDTexto.innerHTML = 'WebSocket 接続済み';
+    };
+    WSC.onclose = function () {
+        console.log('WebSocket Close');
+        var IsCDTextc = document.getElementById('IsSocketConnected');
+        IsCDTextc.innerHTML = 'WebSocket 未接続';
+    }
+    WSC.onmessage = function (e) {
+        var MSData = new Uint8Array(e.data.slice(0, 64));
+        for (var cn in peer.connections) {
+            if (peer.connections[cn][0].open) {
+                console.log("SkyWay WSDataSender : ", peer.connections[cn][0]);
+                peer.connections[cn][0].send(MSData);
+            }
+        }
+        console.log('WebSocket DataGet : ' , MSData);
+    };
+    WSC.onerror = function (error) {
+        console.error('WebSocket エラー : ', error);
+    };
+
+}
+
+window.onunload = function () {
+    peer.destroy();
+    WSC.close();
+};
+
+peer.on('connection', c => {
+    console.log(c);
+    //console.log(peer.connections);
+    c.on('open',()=>{
+        DataConnection[c.remoteID] = c;
+        console.log('SkyWay DataOpen : ', c);
+    });
+    c.on('close',()=>{
+        console.log('SkyWay DataClose : ', c);
+        delete DataConnection[c.remoteID];
+    });
+    c.on('data',function(data){
+        console.log('SkyWay DataGet : ', data);
+        WSC.send(data);
+    });
+  });
+
+  function ReConnect(){
+    console.log('WebSocket 再接続');
+      WSC.close();
+      WSConnecter();
+}
+
+
+
+
+  
