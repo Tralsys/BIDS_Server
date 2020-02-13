8d7786150b5a9c9e12ad064b0b65ee29a3f41f23
ＴＲ <public@technotter.com>
2020-02-13 14:24:58 +0900

    Add Useful function

--- BIDSsv/UFunc.cs
+++ BIDSsv/UFunc.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TR.BIDSsv
 {
@@ -69,112 +71,112 @@ namespace TR.BIDSsv
       return ba;
     }
 
-    public static int GetInt(this byte[] ab)
+    public static int GetInt(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(int);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToInt32(ab, 0);
     }
-    public static float GetFloat(this byte[] ab)
+    public static float GetFloat(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(float);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToSingle(ab, 0);
     }
-    public static double GetDouble(this byte[] ab)
+    public static double GetDouble(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(double);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToDouble(ab, 0);
     }
-    public static bool GetBool(this byte[] ab)
+    public static bool GetBool(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(bool);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToBoolean(ab, 0);
     }
-    public static uint GetUInt(this byte[] ab)
+    public static uint GetUInt(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(uint);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToUInt32(ab, 0);
     }
-    public static short GetShort(this byte[] ab)
+    public static short GetShort(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(short);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToInt16(ab, 0);
     }
-    public static ushort GetUShort(this byte[] ab)
+    public static ushort GetUShort(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(ushort);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToUInt16(ab, 0);
     }
-    public static long GetLong(this byte[] ab)
+    public static long GetLong(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(long);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToInt64(ab, 0);
     }
-    public static ulong GetULong(this byte[] ab)
+    public static ulong GetULong(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(long);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToUInt64(ab, 0);
     }
-    public static char GetChar(this byte[] ab)
+    public static char GetChar(this byte[] ab, int ind = 0)
     {
       int sz = sizeof(char);
 
       byte[] ba = new byte[sz];
       int i = sz - ab.Length;
-      Array.Copy(ab, 0, ba, i < 0 ? 0 : i, sz);
+      Array.Copy(ab, ind, ba, i < 0 ? 0 : i, sz);
       if (isLE) Array.Reverse(ba);
 
       return BitConverter.ToChar(ab, 0);
@@ -191,15 +193,27 @@ namespace TR.BIDSsv
     public static byte[] GetBytes(this in ulong arg) => ToBytes(arg);
     public static byte[] GetBytes(this in char arg) => ToBytes(arg);
 
-    public static int ToInt32(this byte[] ab) => GetInt(ab);
-    public static float ToSingle(this byte[] ab) => GetFloat(ab);
-    public static double ToDouble(this byte[] ab) => GetDouble(ab);
-    public static bool ToBoolean(this byte[] ab) => GetBool(ab);
-    public static uint ToUInt32(this byte[] ab) => GetUInt(ab);
-    public static short ToInt16(this byte[] ab) => GetShort(ab);
-    public static ushort ToUInt16(this byte[] ab) => GetUShort(ab);
-    public static long ToInt64(this byte[] ab) => GetLong(ab);
-    public static ulong ToUInt64(this byte[] ab) => GetULong(ab);
-    public static char ToChar(this byte[] ab) => GetChar(ab);
+    public static int ToInt32(this byte[] ab, int ind = 0) => GetInt(ab, ind);
+    public static float ToSingle(this byte[] ab, int ind = 0) => GetFloat(ab, ind);
+    public static double ToDouble(this byte[] ab, int ind = 0) => GetDouble(ab, ind);
+    public static bool ToBoolean(this byte[] ab, int ind = 0) => GetBool(ab, ind);
+    public static uint ToUInt32(this byte[] ab, int ind = 0) => GetUInt(ab, ind);
+    public static short ToInt16(this byte[] ab, int ind = 0) => GetShort(ab, ind);
+    public static ushort ToUInt16(this byte[] ab, int ind = 0) => GetUShort(ab, ind);
+    public static long ToInt64(this byte[] ab, int ind = 0) => GetLong(ab, ind);
+    public static ulong ToUInt64(this byte[] ab, int ind = 0) => GetULong(ab, ind);
+    public static char ToChar(this byte[] ab, int ind = 0) => GetChar(ab, ind);
+
+    public static int ToInt32(IEnumerable<byte> ab, int ind = 0) => GetInt(ab.ToArray(), ind);
+    public static float ToSingle(IEnumerable<byte> ab, int ind = 0) => GetFloat(ab.ToArray(), ind);
+    public static double ToDouble(IEnumerable<byte> ab, int ind = 0) => GetDouble(ab.ToArray(), ind);
+    public static bool ToBoolean(IEnumerable<byte> ab, int ind = 0) => GetBool(ab.ToArray(), ind);
+    public static uint ToUInt32(IEnumerable<byte> ab, int ind = 0) => GetUInt(ab.ToArray(), ind);
+    public static short ToInt16(IEnumerable<byte> ab, int ind = 0) => GetShort(ab.ToArray(), ind);
+    public static ushort ToUInt16(IEnumerable<byte> ab, int ind = 0) => GetUShort(ab.ToArray(), ind);
+    public static long ToInt64(IEnumerable<byte> ab, int ind = 0) => GetLong(ab.ToArray(), ind);
+    public static ulong ToUInt64(IEnumerable<byte> ab, int ind = 0) => GetULong(ab.ToArray(), ind);
+    public static char ToChar(IEnumerable<byte> ab, int ind = 0) => GetChar(ab.ToArray(), ind);
+
   }
 }
