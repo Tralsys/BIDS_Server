b125dd5acb7aed9ced119ad2043a591ffcb9b274
ＴＲ <public@technotter.com>
2020-05-06 04:53:37 +0900

    ErrorをExceptionでthrowできるように

--- BIDSsv/Errors.cs
+++ BIDSsv/Errors.cs
@@ -23,4 +23,20 @@ namespace TR.BIDSsv
     public static string GetCMD(this ErrorNums ErrNum) => ConstVals.CMD_HEADER + ConstVals.CMD_ERROR + ((int)ErrNum).ToString();
     
   }
+
+  /// <summary>BIDSのコマンド周りのエラーを報告する</summary>
+  public class BIDSErrException : Exception
+  {
+    public Errors.ErrorNums ErrNum { get; }
+    public string ErrCMD { get; }
+
+    public override string Message { get; }
+
+    public BIDSErrException(Errors.ErrorNums errnum)
+    {
+      ErrNum = errnum;
+      ErrCMD = Errors.GetCMD(ErrNum);
+      Message = string.Format("{0} ({1})", ErrCMD, ErrNum);
+    }
+  }
 }
