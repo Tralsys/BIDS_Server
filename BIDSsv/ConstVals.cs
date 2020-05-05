c9efa9cac916afd3348b2e6fb795f6dbe7d123f0
ＴＲ <public@technotter.com>
2020-05-06 04:51:20 +0900

    定数値の追加

--- BIDSsv/ConstVals.cs
+++ BIDSsv/ConstVals.cs
@@ -1,20 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TR.BIDSsv
+﻿namespace TR.BIDSsv
 {
   public static class ConstVals
   {
+    public const string ToStrFormatInt = "D";
+    public const string ToStrFormatFloat = "0.0#####";
+
+
+    public const char BIN_CMD_HEADER_0 = 't';
+    public const char BIN_CMD_HEADER_1 = 'r';
+
+    public const byte BIN_CMD_INFO_DATA = 0x62;
+    public const byte BIN_CMD_PANEL_DATA = 0x70;
+    public const byte BIN_CMD_SOUND_DATA = 0x73;
+
+    public enum BIN_CMD_INFOD_TYPES
+    {
+      SPEC,
+      STATE,
+      BVE5D,
+      OPEND,
+      HANDLE
+    }
+
     public const string CMD_HEADER = "TR";
+    public const string GIPI_HEADER = "TO";
+
+    public const string CRLF = "\r\n";
+
+    /// <summary>Line Feed (\r)</summary>
+    public const string CR_S = "\r";
+    /// <summary>Carriage Return (\n)</summary>
+    public const string LF_S = "\n";
+
+    /// <summary>Line Feed (\r)</summary>
+    public const char CR_C = '\r';
+    /// <summary>Carriage Return (\n)</summary>
+    public const char LF_C = '\n';
 
     public const char CMD_VERSION = 'V';
     public const char CMD_INFOREQ = 'I';
     public const char CMD_AUTOSEND_ADD = 'A';
     public const char CMD_AUTOSEND_DEL = 'D';
     public const char CMD_ERROR = 'E';
+    public const char CMD_REVERSER = 'R';
+    public const char CMD_POWER = 'P';
+    public const char CMD_BREAK = 'B';
+    public const char CMD_SPoleMC = 'S';
+    public const char CMD_KeyCtrl = 'K';
 
     public const char CMD_SEPARATOR = 'X';
+
+    public const int DTYPE_CHAR_POS = 3;//TRIx
+    public const int DNUM_POS = 4;
 
     public const char DTYPE_ELAPD = 'E';
     public const char DTYPE_DOOR = 'D';
@@ -23,11 +60,21 @@ namespace TR.BIDSsv
     public const char DTYPE_PANEL = 'P';
     public const char DTYPE_SOUND = 'S';
 
+    public const char DTYPE_PANEL_ARR = 'p';
+    public const char DTYPE_SOUND_ARR = 's';
+
+    /// <summary>Panel/Soundの連続出力機能で出力する数</summary>
+    public const int PANEL_ARR_PRINT_COUNT = 32;
+    public const int SOUND_ARR_PRINT_COUNT = 32;
+
     public class DNums
     {
       public enum ElapD
       {
-        Distance,
+        Time_HMSms = -3,
+        Pressures = -2,
+        AllData = -1,
+        Distance = 0,
         Speed,
         Time,
         BC_Pres,
@@ -44,14 +91,17 @@ namespace TR.BIDSsv
       }
       public enum HandPos
       {
-        Brake,
+        AllData = -1,
+        Brake = 0,
         Power,
         Reverser,
-        ConstSpd
+        ConstSpd,
+        SelfB
       }
       public enum ConstD
       {
-        Brake_Count,
+        AllData = -1,
+        Brake_Count = 0,
         Power_Count,
         ATSCheckPos,
         B67_Pos,
