20a41bfa62ae42e96da2207c82f3449e51224d2e
ＴＲ <public@technotter.com>
2020-05-10 03:26:41 +0900

    add blankmod

---
+++ BIDSsv.blankmod/BlankMOD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TR.BIDSsv
+{
+	public class BlankMOD : TR.IBIDSsv
+	{
+		public bool IsDisposed { get; private set; }
+		public int Version { get; set; }
+		public string Name { get; private set; }
+		public bool IsDebug { get; set; }
+
+		public bool Connect(in string args)
+		{
+			Name = "BlankMOD" + DateTime.UtcNow.ToString("hhmmssff");
+			Console.WriteLine("BlankMOD Name:{0}", Name);
+			return true;
+		}
+
+		public void Dispose() { IsDisposed = true; }
+
+		public void OnBSMDChanged(in BIDSSharedMemoryData data) { }
+
+		public void OnOpenDChanged(in OpenD data) { }
+
+		public void OnPanelDChanged(in int[] data) { }
+
+		public void OnSoundDChanged(in int[] data) { }
+
+		public void Print(in string data) { }
+
+		public void Print(in byte[] data) { }
+
+		public void WriteHelp(in string args) { }
+	}
+}
