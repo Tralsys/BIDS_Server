d899192b549f010966ccbbd66070ddbe45411e42
ＴＲ <public@technotter.com>
2020-05-11 10:26:47 +0900

    IBIDSsvの最低限の実装を提供

---
+++ IBIDSsv/IBIDSsv_Blank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TR
+{
+	public abstract class IBIDSsv_Blank : IBIDSsv
+	{
+		public bool IsDisposed { get; protected set; }
+		public int Version { get; set; }
+		public string Name { get; protected set; }
+		public bool IsDebug { get; set; }
+
+		public abstract bool Connect(in string args);
+
+		public virtual void OnBSMDChanged(in BIDSSharedMemoryData data) { }
+
+		public virtual void OnOpenDChanged(in OpenD data) { }
+
+		public virtual void OnPanelDChanged(in int[] data) { }
+
+		public virtual void OnSoundDChanged(in int[] data) { }
+
+		public virtual void Print(in string data) { }
+
+		public virtual void Print(in byte[] data) { }
+
+		public abstract void WriteHelp(in string args);
+
+		#region IDisposable Support
+		protected bool disposedValue = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			disposedValue = true;
+		}
+
+		public void Dispose() => Dispose(true);
+		#endregion
+	}
+}
