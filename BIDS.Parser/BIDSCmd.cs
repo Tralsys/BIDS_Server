b736d4c70477f4c20bc5b4ea635d149e50a384fa
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-11 17:56:10 +0900

    `IBIDSCmd`を専用ファイルに移動

--- BIDS.Parser/BIDSCmd.cs
+++ BIDS.Parser/BIDSCmd.cs
@@ -1,36 +1,5 @@
 ﻿namespace BIDS.Parser;
 
-public interface IBIDSCmd
-{
-	Identifier Identifier { get; }
-
-	DataType DataType { get; }
-
-	ReverserPos ReverserPos { get; }
-
-	KeyType KeyType { get; }
-
-	SpecDataType SpecDataType { get; }
-
-	StateDataType StateDataType { get; }
-
-	HandlePosType HandlePosType { get; }
-
-	DoorStateType DoorStateType { get; }
-
-	char RawIdentifier { get; }
-
-	char RawDataType { get; }
-
-	int RequestNumber { get; }
-
-	IReadOnlyCollection<int>? DataInt { get; }
-
-	IReadOnlyCollection<double>? DataDouble { get; }
-
-	ParseError? ParseError { get; }
-}
-
 public record BIDSCmd(
 	Identifier Identifier = Identifier.Unknown,
 	DataType DataType = DataType.Unknown,
---
+++ BIDS.Parser/IBIDSCmd.cs
@@ -0,0 +1,32 @@
+﻿namespace BIDS.Parser;
+
+public interface IBIDSCmd
+{
+	Identifier Identifier { get; }
+
+	DataType DataType { get; }
+
+	ReverserPos ReverserPos { get; }
+
+	KeyType KeyType { get; }
+
+	SpecDataType SpecDataType { get; }
+
+	StateDataType StateDataType { get; }
+
+	HandlePosType HandlePosType { get; }
+
+	DoorStateType DoorStateType { get; }
+
+	char RawIdentifier { get; }
+
+	char RawDataType { get; }
+
+	int RequestNumber { get; }
+
+	IReadOnlyCollection<int>? DataInt { get; }
+
+	IReadOnlyCollection<double>? DataDouble { get; }
+
+	ParseError? ParseError { get; }
+}
