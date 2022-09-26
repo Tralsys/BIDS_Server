e2efca770d56e97b0a178006b094cf3502b890f6
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-27 05:35:13 +0900

    Add `GetBytes` Extension Method

--- BIDS.Parser/Utils/VariableDataTypeExtensions.cs
+++ BIDS.Parser/Utils/VariableDataTypeExtensions.cs
@@ -25,4 +25,25 @@ public static partial class Utils
 
 			_ => null
 		};
+
+	static public byte[] GetBytes(this VariableDataType type, in object? obj)
+		=> type switch
+		{
+			VariableDataType.Boolean => BitConverter.GetBytes((bool)(obj ?? false)),
+
+			VariableDataType.Int8 => BitConverter.GetBytes((sbyte)(obj ?? 0)),
+			VariableDataType.Int16 => BitConverter.GetBytes((short)(obj ?? 0)),
+			VariableDataType.Int32 => BitConverter.GetBytes((int)(obj ?? 0)),
+			VariableDataType.Int64 => BitConverter.GetBytes((long)(obj ?? 0)),
+
+			VariableDataType.UInt8 => BitConverter.GetBytes((byte)(obj ?? 0)),
+			VariableDataType.UInt16 => BitConverter.GetBytes((ushort)(obj ?? 0)),
+			VariableDataType.UInt32 => BitConverter.GetBytes((uint)(obj ?? 0)),
+			VariableDataType.UInt64 => BitConverter.GetBytes((ulong)(obj ?? 0)),
+
+			VariableDataType.Float32 => BitConverter.GetBytes((float)(obj ?? 0)),
+			VariableDataType.Float64 => BitConverter.GetBytes((double)(obj ?? 0)),
+
+			_ => throw new NotSupportedException($"The ValueType `{type} ({(int)type})` is not supported")
+		};
 }
