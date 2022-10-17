b9056fd558a33d562097c40963c227e17f87a6ac
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-10-18 02:10:42 +0900

    Create BIDSServerCore.Disposable.cs

---
+++ BIDSsv/BIDSServerCore.Disposable.cs
@@ -0,0 +1,29 @@
+using System;
+
+using TR.BIDSSMemLib;
+
+namespace TR;
+
+public partial class BIDSServerCore : IDisposable
+{
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+				ClearMod();
+
+				SMem.ReadStop(SMemLib.ARNum.All);
+			}
+
+			disposedValue = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
