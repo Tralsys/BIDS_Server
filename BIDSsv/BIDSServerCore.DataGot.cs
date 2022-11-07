d076b7ed371d771b647d75df012f654e90a63290
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-07 23:34:14 +0900

    switch-caseを使って書き直した

--- BIDSsv/BIDSServerCore.DataGot.cs
+++ BIDSsv/BIDSServerCore.DataGot.cs
@@ -29,34 +29,32 @@ public partial class BIDSServerCore
 	{
 		string? returnCmd = null;
 
-		if (cmd is IBIDSCmd_Info infoCmd)
+		switch (cmd)
 		{
-			if ((infoCmd.DataInt is null || infoCmd.DataInt.Count <= 0)
-				&& (infoCmd.DataDouble is null || infoCmd.DataDouble.Count <= 0))
-			{
-				// データリクエスト型
-				returnCmd = infoCmd.GenerateCommand(SMem.BIDSSMemData, SMem.PanelA, SMem.SoundA);
-			}
-			else
-			{
-				// 何らかのデータを持ったコマンド
-				// => Responseコマンド
-				returnCmd = OnResponseCmdGot.SetValue(SMem, infoCmd);
-			}
-		}
-		else if (cmd is IBIDSCmd_KeyControl or IBIDSCmd_ReverserControl or IBIDSCmd_StandardStyle)
-		{
-			// Response
-			// => 何らかの操作を行った結果の報告コマンド
-			if (cmd is IBIDSCmd_HasDataInt v && v.DataInt?.Count > 0)
-				return null;
+			case IBIDSCmd_Info infoCmd:
+				if ((infoCmd.DataInt is null || infoCmd.DataInt.Count <= 0)
+					&& (infoCmd.DataDouble is null || infoCmd.DataDouble.Count <= 0))
+				{
+					// データリクエスト型
+					returnCmd = infoCmd.GenerateCommand(SMem.BIDSSMemData, SMem.PanelA, SMem.SoundA);
+				}
+				else
+				{
+					// 何らかのデータを持ったコマンド
+					// => Responseコマンド
+					returnCmd = OnResponseCmdGot.SetValue(SMem, infoCmd);
+				}
+				break;
 
-			if (cmd is IStringBIDSCmd scmd)
-				returnCmd = OnControlCmdGot.HandleRequest(SMem, scmd, Version);
-		}
-		else
-		{
-			throw new NotImplementedException();
+			case IBIDSCmd_KeyControl or IBIDSCmd_ReverserControl or IBIDSCmd_StandardStyle:
+				// Response
+				// => 何らかの操作を行った結果の報告コマンド
+				if (cmd is IBIDSCmd_HasDataInt v && v.DataInt?.Count > 0)
+					return null;
+
+				if (cmd is IStringBIDSCmd scmd)
+					returnCmd = OnControlCmdGot.HandleRequest(SMem, scmd, Version);
+				break;
 		}
 
 		if (returnCmd is not null)
