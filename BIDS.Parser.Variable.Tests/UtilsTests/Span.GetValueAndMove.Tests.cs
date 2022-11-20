85db67d804704c87f9d7a59790215cac2db7b5fc
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-20 15:56:24 +0900

    Create Span.GetValueAndMove.Tests.cs
    とりあえずboolとint8のみ実装

---
+++ BIDS.Parser.Variable.Tests/UtilsTests/Span.GetValueAndMove.Tests.cs
@@ -0,0 +1,36 @@
+namespace BIDS.Parser.Variable.Tests.UtilsTests;
+
+public class Span_GetValueAndMoveTests
+{
+	static void TestUtil<T>(byte[] input, T expected, in ReadOnlySpan<byte> afterProcessingSpan, int size, T actual)
+	{
+		Assert.That(actual, Is.EqualTo(expected));
+
+		Assert.That(afterProcessingSpan.ToArray(), Is.EquivalentTo(input[size..]));
+	}
+
+	[TestCase(new byte[] { 0, 1, 2, 3 }, false)]
+	[TestCase(new byte[] { 1, 0, 2, 3 }, true)]
+	[TestCase(new byte[] { 0xFF, 0, 2, 3 }, true)]
+	public void BooleanTest(byte[] input, bool expected)
+	{
+		ReadOnlySpan<byte> bytes = input.ToArray().AsSpan();
+
+		TestUtil(input, expected, bytes,
+			1,
+			Utils.GetBooleanAndMove(ref bytes)
+		);
+	}
+
+	[TestCase(new byte[] { 1, 2, 3, 4 }, (sbyte)1)]
+	[TestCase(new byte[] { 0xFF, 2, 3, 4 }, (sbyte)-1)]
+	public void Int8Test(byte[] input, sbyte expected)
+	{
+		ReadOnlySpan<byte> bytes = input.ToArray().AsSpan();
+
+		TestUtil(input, expected, bytes,
+			1,
+			Utils.GetInt8AndMove(ref bytes)
+		);
+	}
+}
