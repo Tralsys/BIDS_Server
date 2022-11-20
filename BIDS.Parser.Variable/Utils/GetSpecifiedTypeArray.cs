3dc489787a5e8db2349070b7d5b41c16426044c7
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-11-20 23:45:37 +0900

    Create GetSpecifiedTypeArray.cs

---
+++ BIDS.Parser.Variable/Utils/GetSpecifiedTypeArray.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace BIDS.Parser.Variable;
+
+public static partial class Utils
+{
+	static public Array? GetSpecifiedTypeArray(this VariableDataType type, long length)
+		=> type switch
+		{
+			VariableDataType.Boolean => new bool[length],
+
+			VariableDataType.Int8 => new sbyte[length],
+			VariableDataType.Int16 => new short[length],
+			VariableDataType.Int32 => new int[length],
+			VariableDataType.Int64 => new long[length],
+
+			VariableDataType.UInt8 => new byte[length],
+			VariableDataType.UInt16 => new ushort[length],
+			VariableDataType.UInt32 => new uint[length],
+			VariableDataType.UInt64 => new ulong[length],
+
+			VariableDataType.Float16 => new Half[length],
+			VariableDataType.Float32 => new float[length],
+			VariableDataType.Float64 => new double[length],
+
+			_ => null
+		};
+}
