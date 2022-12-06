ef5b3e8766bbd33d7e52cc2d01eac955f0a231b0
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-12-06 12:33:18 +0900

    可変構造データの生成 / 更新機能を追加

--- BIDS_Server.ConsolePrintOnly/Program.cs
+++ BIDS_Server.ConsolePrintOnly/Program.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
+
+using BIDS.Parser;
+using BIDS.Parser.Variable;
+
 using TR;
 
 namespace BIDS_Server.ConsolePrintOnly;
@@ -11,6 +16,8 @@ partial class Program
 		using BIDSServerCore sv = new(10);
 		using ConsolePrintMod Mod01 = new(nameof(Mod01));
 		using ConsolePrintMod Mod02 = new(nameof(Mod02));
+		RandomValueGenerator rand = new();
+		List<VariableStructure> variableStructures = new();
 
 		sv.AddMod(Mod01);
 		sv.AddMod(Mod02);
@@ -21,11 +28,37 @@ partial class Program
 			if (s is null or "exit" or "quit")
 				return;
 
-			if (s.StartsWith("0x"))
+			if (s is "gen")
+			{
+				VariableStructure structure = rand.GetRandomStructure($"Sample{rand.GetInt32()}");
+
+				Log(AppendSMemStructure(new("Creating:\n"), structure.Records));
+
+				IBIDSBinaryData registerCmd = new VariableStructureRegister(structure);
+
+				Mod01.DataGotEventInvoke(registerCmd.GetBytes());
+				variableStructures.Add(structure);
+			}
+			else if (s is "update")
+			{
+				if (variableStructures.Count <= 0)
+				{
+					Log("Cannot UPDATE: `variableStructures` is empty", ConsoleColor.Red);
+					continue;
+				}
+
+				int index = rand.GetUInt16() % variableStructures.Count;
+				VariableStructure structure = variableStructures[index];
+				VariableStructurePayload payload = rand.GetRandomPayload(structure);
+				Log(AppendSMemStructure(new("Updating:\n"), payload.Values));
+
+				IBIDSBinaryData payloadCmd = new VariablePayload(payload, structure);
+				Mod01.DataGotEventInvoke(payloadCmd.GetBytes());
+			}
+			else if (s.StartsWith("0x"))
 				Mod01.DataGotEventInvoke(Convert.FromHexString(s[2..]));
 			else
 				Mod01.DataGotEventInvoke(Encoding.UTF8.GetBytes(s));
 		}
 	}
 }
-
