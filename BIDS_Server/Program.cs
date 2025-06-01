9bc2cb9eaa33ed2044117cf3b91abafc3eaa0519
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2025-06-01 23:48:26 +0900

    バージョンをv012.1に更新

--- BIDS_Server/Program.cs
+++ BIDS_Server/Program.cs
@@ -11,7 +11,7 @@ namespace BIDS_Server
   class Program
   {
     static bool IsLooping = true;
-    const string VerNumStr = "012";
+    const string VerNumStr = "012.1";
     static void Main(string[] args)
     {
       Console.WriteLine("BIDS Server Application");
